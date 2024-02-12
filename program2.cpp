1e6c94f382346b763897cb85ae9444fd786b0a5e
praveengamini <132166319+praveengamini@users.noreply.github.com>
2024-02-12 21:19:48 +0530

    Create program2.cpp

---
+++ program2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    float num1, num2, result;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch(operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error! Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operation. Please enter a valid operation." << endl;
+    }
+
+    return 0;
+}
